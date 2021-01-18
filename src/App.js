6b02f1fb5cec0886aae190e85d6f091000629915
olkhon <alexander.stapel@gmail.com>
2021-01-18 14:59:42 +0100

    select first pic to be current pic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import Axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+const [img, setImg] = useState('');
+
+
+  useEffect (() => {
+    axios.
+    get("https://api.imgflip.com/get_memes")
+    .then((response) => {
+      setImg(response.data.data.memes[0].url);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }, []);
+
+
+
+
   return (
     <div className="App">
      <h1>I can has Memes</h1>
@@ -10,7 +29,10 @@ function App() {
        <button>Send your meme</button>
      </div>
      <div className="holderImage">
-        <img src="" alt="memPic" />
+
+
+        <img src={img} alt="memPic" />
+
      </div>
     </div>
 
